56a9d8d8ba72bf3aa5df910962dc3e8cb56e69b5
umbre <umbrella.spb@hotmail.com>
2025-12-03 20:10:34 +0200

    refactoring

---
+++ ch08/basic/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/google/gopacket/pcap"
+)
+
+func main() {
+	devices, err := pcap.FindAllDevs()
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("=== Список интерфейсов Npcap ===")
+	for _, d := range devices {
+		fmt.Printf("\nName: %s\n", d.Name)
+		fmt.Printf("Desc: %s\n", d.Description)
+		fmt.Println("Addresses:")
+		for _, a := range d.Addresses {
+			fmt.Printf("   IP: %v\n", a.IP)
+		}
+	}
+}
